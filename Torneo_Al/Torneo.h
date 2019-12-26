cf85cfb079d99eb389527d011d17cb6de02939ab
Alemol <alemolamg@users.github.com>
2019-12-26 09:49:29 +0100

    Torneo_Al::ver 2.0 Creando clase Torneo

---
+++ Torneo_Al/Torneo.h
@@ -0,0 +1,19 @@
+/* File:   Torneo.h
+ * Author: Alemol
+ * Created on 26 de diciembre de 2019, 9:47
+ */
+
+#ifndef TORNEO_H
+#define TORNEO_H
+
+class Torneo {
+public:
+    Torneo();
+    Torneo(const Torneo& orig);
+    virtual ~Torneo();
+private:
+
+};
+
+#endif /* TORNEO_H */
+
