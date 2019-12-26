786fd2b1ac8ef6cbce3f6a83588370563acd4c0f
Alemol <alemolamg@users.github.com>
2019-12-26 12:03:42 +0100

    Torneo_Al::ver 2.2 Creando clase Torneo::OrganizaFases

--- Torneo_Al/Torneo.cpp
+++ Torneo_Al/Torneo.cpp
@@ -3,6 +3,8 @@
  * Created on 26 de diciembre de 2019, 9:47
  */
 
+#include <cmath>
+
 #include "Torneo.h"
 
 Torneo::Torneo() {
@@ -12,5 +14,22 @@ Torneo::Torneo(const Torneo& orig) {
 }
 
 Torneo::~Torneo() {
+}
+
+void Torneo::organizaFases(int num) {
+    int ele=1,base=2,resPotencia=2;
+    bool potencia=true;
+    do{
+        if(num<resPotencia){
+            this->tamTorneo=resPotencia;
+            numFases=ele;
+            potencia=false;
+            
+        }else{
+        resPotencia=resPotencia*base;
+        ++ele;
+        }
+           
+    }while(!potencia);
 }
 
