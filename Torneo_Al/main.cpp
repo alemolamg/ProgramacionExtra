a1fd230b3b8dd2d01bba12b60a8de8c4f0c5b120
Alemol <alemolamg@users.github.com>
2019-12-26 09:06:24 +0100

    Torneo_Al::ver 0.0 Creando

---
+++ Torneo_Al/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Alemol
+ * Created on 26 de diciembre de 2019, 8:59
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
