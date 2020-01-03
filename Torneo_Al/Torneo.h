77bd4f83ee59d036df789962a7530e8a5bb659eb
Alemol <alemolamg@users.github.com>
2020-01-03 09:10:26 +0100

    Torneo_Al::ver 2.3 Creando clase Torneo::constructores

--- Torneo_Al/Torneo.h
+++ Torneo_Al/Torneo.h
@@ -7,21 +7,24 @@
 #define TORNEO_H
 
 #include <string>
+#include <vector>
+#include "Player.h"
 
 class Torneo {
 
-    std::string nombreTorneo="Torneo Estandar";
+    std::string nombreTorneo;
+    std::vector<Player> jugadores;
     int numParticipantes;
     int numFases;
     int tamTorneoIni;
-    int tamTorneo; //creo que sobra
+    int tamTorneo;      //creo que sobra
     
 public:
-    Torneo();
+    Torneo(int numPersonas=0,std::string nombre="Torneo Estandar");
     Torneo(const Torneo& orig);
     virtual ~Torneo();
 private:
-    void organizaFases(int num);
+    void organizaFases();
 
 };
 
